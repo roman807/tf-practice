b0e0cc30ec05022b48fde5bedb550891a6410c51
roman807 <roman.moser@gmx.net>
2021-03-05 08:28:28 +0100

    added cnn for time series

---
+++ models/m40_rnn_min_temp.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+
+
+class Model:
+    def __init__(self, model_type, window_size):
+        self.window_size = window_size
+        self.model = None
+        if model_type == 'rnn':
+            self.model = self.model_rnn()
+        elif model_type == 'cnn':
+            self.model = self.model_cnn()
+
+    def get_model(self):
+        return self.model
+
+    def model_rnn(self):
+        return tf.keras.Sequential([
+            tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, -1), input_shape=[self.window_size]),
+            # tf.keras.layers.Reshape((-1, 1), input_shape=[None]),
+            tf.keras.layers.LSTM(50, activation='relu', return_sequences=True),
+            tf.keras.layers.LSTM(50, activation='relu', return_sequences=False),
+            tf.keras.layers.Dense(100, activation='relu'),
+            tf.keras.layers.Dense(1),
+            tf.keras.layers.Lambda(lambda x: x * 10.0)
+        ])
+
+    def model_cnn(self):
+        # trains quicker, better results
+        return tf.keras.Sequential([
+            tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, -1), input_shape=[self.window_size]),
+            tf.keras.layers.Conv1D(20, kernel_size=3, activation='relu'),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(100, activation='relu'),
+            tf.keras.layers.Dense(1),
+            tf.keras.layers.Lambda(lambda x: x * 10.0)
+        ])
--- r40_min_temp.py
+++ r40_min_temp.py
@@ -7,13 +7,20 @@ from time import time
 from datetime import datetime
 
 from tfutils import windowed_dataset, MyCallback
-from models import rnn_min_temp
+from models import m40_rnn_min_temp
 
 
 TRAIN_SPLIT = .75
-N_EPOCHS = 10
+N_EPOCHS = 25
 WINDOW_SIZE = 5
-MODEL_NAME = 'rnn_' + str(int(time()))
+MODEL = 'rnn'
+MODEL_NAME = MODEL + '_' + str(int(time()))
+SAVE_MODEL = True
+PLOTS = True
+
+METRIC = 'mae'
+TARGET_CALLBACK = 1.5
+TARGET_SAVE = 3
 
 
 def main():
@@ -32,33 +39,44 @@ def main():
     ds_test = windowed_dataset(temps[split:], window_size=WINDOW_SIZE, batch_size=20, shuffle=False)
 
     # (2) train model
-    model = rnn_min_temp.get_model()
+    model = m40_rnn_min_temp.Model(MODEL, WINDOW_SIZE).get_model()
     callbacks = MyCallback(metric='mae', greater=False, target=1.5)
     model.summary()
     opt = tf.keras.optimizers.Adam(lr=1e-5)
     model.compile(optimizer=opt, loss='mae', metrics=['mae'])
+    start = time()
     history = model.fit(ds_train, validation_data=ds_test, epochs=N_EPOCHS, callbacks=[callbacks], verbose=1)
+    training_time = np.round(time() - start, 2)
 
     # predict on test data:
     y_pred = model.predict(ds_test).flatten()
     dates_test = dates[split+WINDOW_SIZE:]
     y_test = temps[split+WINDOW_SIZE:]
     mae = np.round(tf.keras.metrics.mae(y_test, y_pred).numpy(), 5)
+    print('----------')
+    print('trained ' + MODEL + ' for ' + str(N_EPOCHS) + ' epochs in ' + str(training_time) + 'seconds')
+    print('testing mae=' + str(np.round(mae, 5)) + '\n')
 
-    # plot training
-    plt.plot(history.epoch, history.history['loss'])
-    plt.plot(history.epoch, history.history['val_loss'], color='orange')
-    plt.title('training loss')
-    plt.savefig('results/' + MODEL_NAME + '_loss.png')
-    # plt.show()
+    # save model
+    if mae < TARGET_SAVE and SAVE_MODEL:
+        model.save('saved_models/' + MODEL_NAME)
+        print('saved model as {}'.format(MODEL_NAME))
 
-    # plot results
-    plt.plot(dates_test, y_test)
-    plt.plot(dates_test, y_pred, color='orange')
-    plt.title('mean absolute error: {}'.format(mae))
-    plt.savefig('results/' + MODEL_NAME + '_results.png')
-    # plt.show()
-    # a=1
+    if PLOTS:
+        # plot training
+        plt.plot(history.epoch, history.history['loss'])
+        plt.plot(history.epoch, history.history['val_loss'], color='orange')
+        plt.title('training loss')
+        plt.savefig('results/' + MODEL_NAME + '_loss.png')
+        print('saved plot: ' + 'results/' + MODEL_NAME + '_loss.png')
+
+        # plot results
+        plt.plot(dates_test, y_test)
+        plt.plot(dates_test, y_pred, color='orange')
+        plt.title('mean absolute error: {}'.format(mae))
+        plt.savefig('results/' + MODEL_NAME + '_results.png')
+        print('saved plot: ' + 'results/' + MODEL_NAME + '_results.png')
+    print('----------')
 
 
 if __name__ == '__main__':
