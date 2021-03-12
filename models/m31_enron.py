1f0c9c8f8713fdeb8d79043980366aac7833d256
roman807 <roman.moser@gmx.net>
2021-03-12 07:33:04 +0100

    completed enron next word predictor

--- models/m31_enron.py
+++ models/m31_enron.py
@@ -8,6 +8,8 @@ class Model:
         self.model = None
         if model_type == 'lstm1':
             self.model = self.model_lstm1()
+        if model_type == 'lstm_bd2':
+            self.model = self.model_lstm_bd2()
 
     def get_model(self):
         return self.model
@@ -16,6 +18,15 @@ class Model:
         return tf.keras.Sequential([
             tf.keras.layers.Embedding(self.vocab_size, self.embed_size),
             tf.keras.layers.LSTM(50, activation='tanh', return_sequences=False),
+            tf.keras.layers.Dense(40, activation='relu'),
+            tf.keras.layers.Dense(self.vocab_size, activation='softmax')
+        ])
+
+    def model_lstm_bd2(self):
+        return tf.keras.Sequential([
+            tf.keras.layers.Embedding(self.vocab_size, self.embed_size),
+            tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, activation='tanh', return_sequences=True)),
+            tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32, activation='tanh', return_sequences=False)),
             tf.keras.layers.Dense(40, activation='relu'),
             tf.keras.layers.Dense(self.vocab_size, activation='softmax')
         ])
--- r31_enron.py
+++ r31_enron.py
@@ -4,17 +4,19 @@ import tensorflow_datasets as tfds
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
+import matplotlib.pyplot as plt
 
 from models import m31_enron
 
-NUM_WORDS = 1000
+NUM_WORDS = 5000
 EMBED_SIZE = 50
 MAX_LEN = 10
-N_EPOCHS = 1
+N_EPOCHS = 3
 
+MODEL = 'lstm1'
 N_EMAILS_TRAIN = 4_000
 N_EMAILS_TEST = 1_000
-MAX_LEN_EMAIL = 40
+MAX_LEN_EMAIL = 30
 
 
 def ds_to_texts(ds, n_emails):
@@ -26,38 +28,55 @@ def ds_to_texts(ds, n_emails):
     return sentences
 
 
+def get_input_sequences(sentences, tokenizer):
+    seq = tokenizer.texts_to_sequences(sentences)
+    input_sequences = []
+    for seq in seq:
+        for i in range(1, min(len(seq)+1, MAX_LEN_EMAIL) - 1):
+            input_sequences.append(seq[:i+1])
+    seq = np.array(pad_sequences(input_sequences, maxlen=MAX_LEN, padding='pre', truncating='pre'))
+    return seq[:, :-1], seq[:, -1]
+
+
+def generate_text(text, tokenizer, model):
+    for i in range(30):
+        input_ = pad_sequences(tokenizer.texts_to_sequences([text]), maxlen=MAX_LEN, padding='pre', truncating='pre')
+        prediction = model.predict(input_)[0]
+        token = np.random.choice(len(prediction), p=prediction)
+        text.append(tokenizer.sequences_to_texts([[token]])[0])
+        # text.append(tokenizer.sequences_to_texts([np.argmax(model.predict(input_), axis=-1)])[0])
+    print(' '.join(text))
+
+
 def main():
     # (1) prepare data
     (train_ds, test_ds), info = tfds.load('aeslc', split=['train', 'test'], with_info=True)
     train_sentences = ds_to_texts(train_ds, N_EMAILS_TRAIN)
     test_sentences = ds_to_texts(test_ds, N_EMAILS_TEST)
-    tokenizer = Tokenizer(NUM_WORDS, lower=True, oov_token='oov')
+    # tokenizer = Tokenizer(NUM_WORDS, lower=True, oov_token='oov')
+    tokenizer = Tokenizer(NUM_WORDS, lower=True)
     tokenizer.fit_on_texts(train_sentences)
-    train_seq = tokenizer.texts_to_sequences(train_sentences)
-    test_seq = tokenizer.texts_to_sequences(test_sentences)
 
-    input_sequences = []
-    for seq in train_seq:
-        for i in range(1, MAX_LEN_EMAIL - 1):
-            input_sequences.append(seq[:i+1])
+    x_train, y_train = get_input_sequences(train_sentences, tokenizer)
+    x_test, y_test = get_input_sequences(test_sentences, tokenizer)
 
-    train_seq = np.array(pad_sequences(input_sequences, maxlen=MAX_LEN, padding='pre', truncating='pre'))
-    x_train, y_train = train_seq[:, :-1], train_seq[:, -1]
-
-    model = m31_enron.Model('lstm1', NUM_WORDS, EMBED_SIZE).get_model()
+    model = m31_enron.Model(MODEL, NUM_WORDS, EMBED_SIZE).get_model()
     model.summary()
-    opt = tf.keras.optimizers.Adam(lr=1e-4)
+    opt = tf.keras.optimizers.Adam(lr=1e-2)
     model.compile(optimizer=opt, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    model.fit(x_train, y_train, epochs=N_EPOCHS, batch_size=20, verbose=1)
+    history = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=N_EPOCHS, batch_size=20, verbose=1)
 
-    #todo: check softmax (should predict on 1000 classes)
-    text = ['i', 'hope', 'you']
-    for i in range(30):
-        input_ = pad_sequences(tokenizer.texts_to_sequences([text]), maxlen=MAX_LEN, padding='pre', truncating='pre')[0]
-        text.append(tokenizer.sequences_to_texts(model.predict_classes(input_)))
-    print(text)
+    plt.plot(history.epoch, history.history['loss'])
+    plt.plot(history.epoch, history.history['val_loss'], color='orange')
+    plt.show()
+
+    generate_text(['i', 'hope', 'you'], tokenizer, model)
+    generate_text(['try', 'something'], tokenizer, model)
+    generate_text(['please'], tokenizer, model)
+    generate_text(['please', 'follow'], tokenizer, model)
+    generate_text(['please', 'follow', 'up', 'with'], tokenizer, model)
+    generate_text(['why'], tokenizer, model)
 
 
 if __name__ == '__main__':
     main()
-
