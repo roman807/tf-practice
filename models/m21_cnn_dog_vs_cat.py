3509ac79947e0d178b3c59bc2aecc8232aa1ac14
roman807 <roman.moser@gmx.net>
2021-03-16 07:38:17 +0100

    dog vs cat cnn

---
+++ models/m21_cnn_dog_vs_cat.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+
+
+class Model:
+    def __init__(self, model_type, input_shape):
+        self.input_shape = input_shape
+        self.model = None
+        if model_type == 'cnn':
+            self.model = self.model_cnn()
+        if model_type == 'cnn_large':
+            self.model = self.model_cnn_large()
+
+    def get_model(self):
+        return self.model
+
+    def model_cnn(self):
+        return tf.keras.Sequential([
+            # tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, -1), input_shape=self.input_shape),
+            tf.keras.layers.Conv2D(40, kernel_size=(5, 5), activation='relu', input_shape=self.input_shape),
+            tf.keras.layers.MaxPool2D((2, 2)),
+            tf.keras.layers.Conv2D(40, kernel_size=(3, 3), activation='relu'),
+            tf.keras.layers.MaxPool2D((2, 2)),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(50, activation='relu'),
+            tf.keras.layers.Dense(2, activation='softmax')
+        ])
+
+    def model_cnn_large(self):
+        return tf.keras.Sequential([
+            # tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, -1), input_shape=self.input_shape),
+            tf.keras.layers.Conv2D(80, kernel_size=(5, 5), activation='relu', input_shape=self.input_shape),
+            tf.keras.layers.MaxPool2D((2, 2)),
+            tf.keras.layers.Conv2D(80, kernel_size=(3, 3), activation='relu'),
+            tf.keras.layers.MaxPool2D((2, 2)),
+            tf.keras.layers.Conv2D(20, kernel_size=(3, 3), activation='relu'),
+            tf.keras.layers.MaxPool2D((2, 2)),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(100, activation='relu'),
+            tf.keras.layers.Dense(2, activation='softmax')
+        ])
---
+++ r21_dog_vs_cat.py
@@ -0,0 +1,71 @@
+# to download data to remote machine with kaggle api:
+# (1) Log in to machine: ssh [user]@[IP-address]
+# (2) Activate env, e.g. conda activate /anaconda/envs/py37_tensorflow
+# (3) pip install kaggle
+# (4) Go to kaggle.com -> account -> create token
+# (5) (log out of remote ->) scp kaggle.json dsvm-user@[IP-address]:/home/[user]/.kaggle
+# (6) (log in to remote ->) chmod 600 /home/[user]/.kaggle/kaggle.json
+# (7) kaggle datasets download -d chetankv/dogs-cats-images
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+from time import time
+import matplotlib.pyplot as plt
+
+from models import m21_cnn_dog_vs_cat
+from tfutils import MyCallbackStopTraining
+
+
+N_EPOCHS = 5
+MODEL = 'cnn_large'
+MODEL_NAME = MODEL + '_' + str(int(time()))
+SAVE_MODEL = True
+PLOTS = True
+
+TARGET_ACC_CALLBACK = .9
+TARGET_ACC_SAVE = .8
+
+
+# next: image augmentation, loop for hyper-parameter search
+def main():
+    # (1) prepare data:
+    train_dir = 'data/dog_vs_cat/dataset/training_set'
+    test_dir = 'data/dog_vs_cat/dataset/test_set'
+
+    data_gen = ImageDataGenerator(rescale=1/255.0)
+    train_data_gen = data_gen.flow_from_directory(train_dir, target_size=(28, 28), batch_size=20)
+    test_data_gen = data_gen.flow_from_directory(test_dir, target_size=(28, 28), batch_size=20)
+
+    # (2) train model:
+    model = m21_cnn_dog_vs_cat.Model(MODEL, input_shape=(28, 28, 3)).get_model()
+    model.summary()
+    opt = tf.keras.optimizers.Adam(lr=1.0e-3)
+    model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+    callback = MyCallbackStopTraining(target=TARGET_ACC_CALLBACK)
+    start = time()
+    history = model.fit(train_data_gen, validation_data=test_data_gen, epochs=N_EPOCHS, callbacks=[callback], verbose=1)
+    training_time = np.round(time() - start, 2)
+
+    # (3) predict on test data:
+    loss, acc = model.evaluate(test_data_gen)
+    print('----------')
+    print('trained ' + MODEL + ' for ' + str(N_EPOCHS) + ' epochs in ' + str(training_time) + 'seconds')
+    print('testing acc=' + str(np.round(acc, 5)) + '\n')
+
+    # (4) save model, plots:
+    if acc > TARGET_ACC_SAVE and SAVE_MODEL:
+        model.save('saved_models/' + MODEL_NAME)
+        print('saved model as {}'.format(MODEL_NAME))
+
+    if PLOTS:
+        plt.plot(history.epoch, history.history['loss'])
+        plt.plot(history.epoch, history.history['val_loss'], color='orange')
+        plt.title('training loss')
+        plt.savefig('results/' + MODEL_NAME + '_loss.png')
+        print('saved plot: ' + 'results/' + MODEL_NAME + '_loss.png')
+    print('----------')
+
+
+if __name__ == '__main__':
+    main()
